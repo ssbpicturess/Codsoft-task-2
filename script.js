a08eff09491e7a26aa1d8308a5907a1124cb94d8
Saurabh Singh <108174081+ssbpicturess@users.noreply.github.com>
2024-06-27 11:14:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// // NOTES OF JAVASCRIPT
+
+// STRING METHOD : string method ka use string ko manupulate karne k liye kiya jata hai . iske bhtt sare method hai 
+
+// 1 (str.length)  property string ki length find krne k liye use krte hai. 
+// 2 (str.toUpperCase()) this method are used to change the string into uppercase.
+// 3 (str.toLowerCase()) this method are used to change the string into LowerCase. 
+// 4 (str.trim()) this method are used to remove whitespace in string from start to end. 
+// 5 (str.slice(2,4)) basically slice method takes some arg and this method is use used to remove some slice of string 
+//                    ex--> 2,4 hai isme 2 se 4 tk k index wale string remove ho jayenge but 4 ko chhodke last arg include nhi hota hai. 
+// 6 (str.slice(3))  same as slice ek arg wale me uske baad k jitne index hote hai sb remove ho jate hai. 
+
+// let name = saurabh;
+// 7 (name.concat("My name is ", name)) this method is used to adding two or more values of variable. 
